270e04a16477be7e228c464b901fb11f6587671a
lDragZ <168339403+lDragZ@users.noreply.github.com>
2024-05-27 18:23:25 +0200

    Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia meuNokia = new Nokia("111-654-0000", "Nokia 0010", "Nokia", 17);
+        Iphone meuIphone = new Iphone("222-456-0000", "iPhone 12", "Apple", 63);
+
+        meuNokia.Ligar();
+        meuNokia.ReceberLigacao();
+        meuNokia.InstalarAplicativo("Telegram");
+
+        meuIphone.Ligar();
+        meuIphone.ReceberLigacao();
+        meuIphone.InstalarAplicativo("WhatsApp");
+    }
+}
